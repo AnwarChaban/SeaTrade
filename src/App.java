a92f530bdf834ccff75f7726653ff2b531043ca4
ali.al-wazny <ali.al-wazny@flagbit.de>
2024-02-06 10:43:10 +0100

    feat: Instanciate and register Ship programmatically

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import object.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new Ship("ship", "company").instantiate();
+        System.out.println("New Ship instance!");
     }
 }
