36d4ceafb10a4ffa0d6fb5827b23751020c0bd9a
ali.al-wazny <ali.al-wazny@flagbit.de>
2024-02-06 11:35:36 +0100

    feat: Establish connection and register company

--- src/App.java
+++ src/App.java
@@ -2,6 +2,9 @@ import object.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        new Company("company").instantiate();
+        System.out.println("New Company instance!");
+
         new Ship("ship", "company").instantiate();
         System.out.println("New Ship instance!");
     }
