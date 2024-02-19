e573f406eb4acf95a1539025d61b4220e7c29df7
ali.al-wazny <ali.al-wazny@flagbit.de>
2024-02-19 15:42:22 +0100

    fix: Import arraylist and rename some variables

--- src/object/Company.java
+++ src/object/Company.java
@@ -2,34 +2,33 @@ package object;
 
 import helper.communication.*;
 import java.io.IOException;
+import java.util.*;
 
 public class Company {
     String name;
-    ArrayList<Ship> shipList = new ArrayList<Ship>;
+    ArrayList<Ship> shipList = new ArrayList<Ship>();
 
     public Company(String name) {
         this.name = name;
     }
 
     public void instantiate() {
+        // connect to seaTrade server
         Client seaTrade = new Client(8150, "localhost");
         new Thread(seaTrade).run();
         
 
-        Client ship = new Client(8150, "localhost");
-        new Thread(ship).run();
-        
         try {
             seaTrade.send(String.format("register:%s", this.name));
-            System.out.println("Server: " + client.receive());
-            
-//            client.stop();
+            System.out.println("Server: " + seaTrade.receive());
+            instantiateShip("ship1");
+            seaTrade.stop();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private Ship instantiateShip(String shipName) {
+    private void instantiateShip(String shipName) {
         Ship ship = new Ship(shipName, this.name).instantiate("plymouth");
         shipList.add(ship);
     }
