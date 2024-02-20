989c6ff4764edd6ba33278f776aded32c2b0e3e8
anwar.chaban <anwar.chaban@ktc.de>
2024-02-20 11:39:50 +0100

    save company date in database

--- src/object/Company.java
+++ src/object/Company.java
@@ -2,26 +2,36 @@ package object;
 
 import helper.communication.*;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.*;
 
 public class Company {
     String name;
+    private String id;
+    private int deposit;
+    private Datenbank db;
     ArrayList<Ship> shipList = new ArrayList<Ship>();
 
     public Company(String name) {
         this.name = name;
     }
 
-    public void instantiate() {
+    public void instantiate() throws SQLException {
         // connect to seaTrade server
         Client seaTrade = new Client(8150, "localhost");
+        db = new Datenbank();
         new Thread(seaTrade).run();
         
 
         try {
             seaTrade.send(String.format("register:%s", this.name));
+            String deposit = seaTrade.receive();
+            String deposits[] = deposit.split(":");
+            this.id = genarateId();
+            this.deposit = Integer.parseInt(deposits[2]); // Convert string to int
+            db.setCompany(this.id, this.name, Integer.toString(this.deposit));
+            // instantiateShip("ship1");
             System.out.println("Server: " + seaTrade.receive());
-            instantiateShip("ship1");
             seaTrade.stop();
         } catch (IOException e) {
             e.printStackTrace();
@@ -31,5 +41,10 @@ public class Company {
     private void instantiateShip(String shipName) {
         Ship ship = new Ship(shipName, this.name).instantiate("plymouth");
         shipList.add(ship);
+    }
+
+    private String genarateId() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
     }
 }
---
+++ src/object/Datenbank.java
@@ -0,0 +1,40 @@
+package object;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Datenbank {
+    private Connection connection;
+    private String query;
+
+    public Datenbank() {
+        // TODO: Implement constructor
+        connection = new SqlConnection().getConnection();
+    }
+
+    public void execQuery() {
+        // TODO: Implement execQuery method
+        //
+    }
+
+    public void execStatement() {
+        // TODO: Implement execStatement method
+    }
+
+    public void setCompany(String compnayId, String companyName, String deposit) throws SQLException {
+        
+        String insertQuery = "INSERT IGNORE INTO company (ID,CompanyName,Guthaben) VALUES (?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(insertQuery);
+            
+
+        if (!compnayId.equals("null")) {
+            statement.setString(1, compnayId);
+            statement.setString(2, companyName);
+            statement.setString(3, deposit);
+            statement.executeUpdate();
+        }
+        System.out.println("Company inserted!");
+    }
+}
+ 
