b720498acf499d7430f734a5cb685c7ece46c60f
chickensoup2077 <103024215+chickensoup2077@users.noreply.github.com>
2024-02-26 15:35:57 +0100

    hafen

--- src/object/Hafen.java
+++ src/object/Hafen.java
@@ -1,9 +1,12 @@
+import java.util.List;
+;
 
 package object;
 
 public class Hafen {
     private int cargoCount;
     private String id;
+}
 
     public Hafen(String id) {
         this.id = id;
@@ -20,19 +23,12 @@ public class Hafen {
     public int getReservedCargoCount() {
         // Add your implementation here
     }
+
     public void setReservedCargoCount(int reservedCargoCount) {
         this.reservedCargoCount = reservedCargoCount;
-    }
-
-    public int getReservedCargoCount() {
-        return reservedCargoCount;
-    }
-
-    public void setReservedCargoCount(int reservedCargoCount) {
-        // Add your implementation here
     }
 
     public List<Ship> getShips() {
         // Add your implementation here
     }
-}
+
